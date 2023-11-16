5fca1e01d995e12e8880ed7c83b59e6fa639777c
Maurice <mauriceglenn2@gmail.com>
2023-11-16 11:00:34 -0600

    creating 10 posts

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,30 +4,61 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
 function Post() {
-    const { id } = useParams()
-    const [posts, setPosts] = useState([]) 
+  const { id } = useParams()
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true) // loading is true by default, as soon as this mounts its going to be loading
 
 
-    console.log(id) // {id: "1"} in console, with object. Get the id from the path
-    // in the Route path=":id" in App.js
+  console.log(id) // {id: "1"} in console, with object. Get the id from the path
+  // in the Route path=":id" in App.js
 
-    //Can't use this for placing HTML in the page, need to use useState
-   useEffect(() => {
+  //Can't use this for placing HTML in the page, need to use useState
+  useEffect(() => {
     async function fetchPost() {
       const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`) // Promise {<pending>}
-        console.log(data) // Data is unlocked
-        setPosts(data)
+      console.log(data) // Data is unlocked
+      setPosts(data)
+      setLoading(false) //soon as it mounts its going to stop loading
     }
-    fetchPost()   
-   }, [])
+    fetchPost()
+  }, [])
 
 
   return (
-    <div>
-        {/* map over every single post */}
-       {posts.map(post => <div>{post.id}</div>)}
-    </div>
+    <>
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input
+            type="number"
+          />
+          <button>Enter</button>
+        </div>
+      </div>
+      {loading
+        ? new Array(10).fill(0).map((_, index) => (
+          <div className="post" key={index}>
+            <div className="post__title">
+              <div className="post__title--skeleton"></div>
+            </div>
+            <div className="post__body">
+              <p className="post__body--skeleton"></p>
+            </div>
+          </div>
+        )) : (
+          posts.map(post => (
+            <div className="post" key={post.id}>
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+
+          ))
+        )}
+    </>
   )
 }
+
+//21:47
 
 export default Post
